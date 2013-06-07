58bbac5db9af02874c7ce8f9c44c653830836a3c
encelades <neodima@live.com>
2013-06-07 18:46:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,92 @@
+ <!DOCTYPE>
+    <html>
+    <head>
+    <meta charset="utf-8" />
+    <title>index</title>
+    <link rel="stylesheet" type="text/css" href="totocss/bootstrap.css"> 
+      <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    
+    <body>
+    
+    
+        <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+        <a class="brand " href="#">toto</a>
+        <ul class="nav">
+        <li><a class=" active" href="index.php">Accueil</a></li>
+        <li><a href="page4.php">Enregistrement</a></li>
+        <li><a href="#">Recherche</a></li>
+        <li><a href="#">Deconnexion</a></li>
+        </ul>
+        </div>
+        </div>               
+    <div id="corp">
+    <p align="justify">bonjour  Nunc dignissim rutrum dignissim. Maecenas vitae mi odio, quis egestas lorem. 
+                        Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada eget.
+                        Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada eget.
+                    Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada eget.
+                    Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada egeNunc dignissim rutrum dignissim. Maecenas vitae mi odio, quis egestas lorem. 
+                        Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada eget.
+                        Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada eget.
+                    Vestibulum elementum pharetra odio, id pellentesque purus rhoncus sit amet. Morbi tincidunt viverra magna, at auctor nibh malesuada eget.</p>
+    
+        <table class="table table-hover">
+       <tr>
+           
+       <td>
+       Materiel:
+       </td>
+       <td> 
+       
+       Total:
+       </td>
+       
+       </tr>
+       
+       <tr>
+       <td>
+       Ordinateur:
+       </td>
+       
+       <td>
+       ''
+       </td>
+       
+       </tr>
+       <tr>
+       <td>
+       Scanner:
+       </td>
+       <td>
+       ''
+       </td>
+       
+       </tr>
+       
+       <tr>
+       <td>
+       Switch:
+       </td>
+       <td>
+       ''
+       </td>
+       
+       </tr>
+       <td>
+       imprimante:
+       </td>
+       <td>
+       ''
+       </td>
+    
+        </table>
+    
+    
+    </div>
+    
+    
+    
+    
+    </body>
+    </html>
